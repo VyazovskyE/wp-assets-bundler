87e8ed45982cf362a17d055c035eccc8f4ed5738
evgeniy <krauzerfy@gmail.com>
2020-12-01 20:43:59 +0200

    added sass loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,5 @@
 const paths = require('./paths.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: paths.src + '/scripts/main.js',
@@ -20,6 +20,14 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {loader: 'file-loader', options: {name: '[name].[ext]'}},
+          {loader: 'css-loader', options: {sourceMap: true, importLoaders: 1}},
+          {loader: 'sass-loader', options: {sourceMap: true}}
+        ]
       },
     ]
   }

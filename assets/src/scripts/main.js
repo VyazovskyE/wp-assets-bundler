19260b35c66341b1d6b75faca82084df40f76eda
evgeniy <krauzerfy@gmail.com>
2020-12-01 20:30:49 +0200

    added babel loader

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('Hello >>>')
+
+async function test () {
+  const res = await fetch('/')
+  return await res.json()
+}
+
+test().then(console.log)
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,8 +1,26 @@
 const paths = require('./paths.js')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: paths.src + '/scripts/main.js',
   output: {
     path: paths.build,
   },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  }
 }
